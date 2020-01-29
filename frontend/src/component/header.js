347bf4c4e37caa4373a16e4c997409bac3c259ad
cchen980 <chao_chen@comcast.com>
2020-01-29 16:17:25 -0500

    clear folder and simple header

--- frontend/src/component/header.js
+++ frontend/src/component/header.js
@@ -1,49 +1,77 @@
 import React from 'react';
-import { Button } from 'antd';
+import {makeStyles} from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import {Container} from "@material-ui/core";
+import GitHubIcon from '@material-ui/icons/GitHub';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import Link from "@material-ui/core/Link";
+import HomeIcon from '@material-ui/icons/Home';
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        flexGrow: 1,
+    },
+    menuButton: {
+        marginRight: theme.spacing(2),
+    },
+    title: {
+        flexGrow: 1,
+    },
+    container: {
+        display: 'flex',
+        alignItems: 'center',
+        '& > *': {
+            margin: theme.spacing(1),
+        },
+        fontFamily: 'Arial',
+        fontSize: 20
+    }
+}));
+
+function ButtonAppBar() {
+    const classes = useStyles();
+    const preventDefault = event => event.preventDefault();
+    return (
+        <div className={classes.root}>
+            <AppBar position="static" color="inherit">
+                <Toolbar>
+                    <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                        <HomeIcon />
+                    </IconButton>
+                    <Typography variant="h6" className={classes.title}>
+                        Title
+                    </Typography>
+                    <Container maxWidth="sm" className={classes.container}>
+                        <Link href="#" color='inherit' onClick={preventDefault}>
+                            Home
+                        </Link>
+                        <Link href="#" color='inherit' onClick={preventDefault}>
+                            About
+                        </Link>
+                        <Link href="#" color='inherit' onClick={preventDefault}>
+                            Contact
+                        </Link>
+                    </Container>
+                    <IconButton>
+                        <GitHubIcon/>
+                    </IconButton>
+                    <IconButton>
+                        <LinkedInIcon/>
+                    </IconButton>
+                </Toolbar>
+            </AppBar>
+        </div>
+    );
+}
 
 class Header extends React.Component {
     render() {
-        if (this.props.data) {
-            const name = this.props.data.name;
-            const occupation = this.props.data.occupation;
-            const description = this.props.data.description;
-            const city = this.props.data.address.city;
-        }
-
         return (
             <header id="home">
-            <div className="App">
-                <Button type="primary">Button</Button>
-            </div>
-            <nav id="nav-wrap">
-
-            <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
-                <a className="mobile-btn" href="#home" title="Hide navigation">Hide navigation</a>
-
-            <ul id="nav" className="nav">
-                <li className="current"><a className="smoothscroll" href="#home">Home</a></li>
-                <li><a className="smoothscroll" href="#about">About</a></li>
-                <li><a className="smoothscroll" href="#resume">Resume</a></li>
-                <li><a className="smoothscroll" href="#contact">Contact</a></li>
-            </ul>
-
-            </nav>
-
-            <div className="row banner">
-            <div className="banner-text">
-                {/* <h1 className="responsive-headline">I'm {name}.</h1> */}
-                {/* <h3>I'm a {city} based <span>{occupation}</span>. {description}.</h3> */}
-                <hr />
-                <ul className="social">
-                    {/* {networks} */}
-                </ul>
-            </div>
-            </div>
-
-            <p className="scrolldown">
-            <a className="smoothscroll" href="#about"><i className="icon-down-circle"></i></a>
-            </p>
-
+                <ButtonAppBar/>
             </header>
         );
     }
